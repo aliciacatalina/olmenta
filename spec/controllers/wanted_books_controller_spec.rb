349e2007a5b95562e5f144eb81703ac4756a9e82
Alicia <alicia@icalialabs.com>
2014-07-21 18:30:58 -0500

    Adds spec files for wanted books

---
+++ spec/controllers/wanted_books_controller_spec.rb
@@ -0,0 +1,5 @@
+require 'rails_helper'
+
+RSpec.describe WantedBooksController, :type => :controller do
+
+end
---
+++ spec/factories/wanted_books.rb
@@ -0,0 +1,6 @@
+# Read about factories at https://github.com/thoughtbot/factory_girl
+
+FactoryGirl.define do
+  factory :wanted_book do
+  end
+end
